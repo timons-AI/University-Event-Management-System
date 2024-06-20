905ab7df6f3cd0116411c3bb17b792ced48cdb86
timons-AI <123802219+timons-AI@users.noreply.github.com>
2024-06-20 15:04:06 +0300

    dashboard overview

--- auth-tutorial/app/(dashboard)/(routes)/admin/dashboard/page.tsx
+++ auth-tutorial/app/(dashboard)/(routes)/admin/dashboard/page.tsx
@@ -15,6 +15,15 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FcDocument } from "react-icons/fc";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 const Admin = async () => {
   const user = await currentUser();
   if (!user) {
@@ -34,51 +43,300 @@ const Admin = async () => {
     <div className=" p-6 space-y-4">
       <FormSuccess message={"Welcome back, " + user.name + " !"} />
       <div className=" grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <InfoCard
-          icon={User2Icon}
-          variant="emerald"
-          label="Leaders"
-          numberOfItems={
-            users.filter((users) => !users.role || users.role === "LEADER")
-              .length
-          }
-        />
-        <InfoCard
-          icon={User2Icon}
-          variant="rose"
-          label="Students"
-          numberOfItems={
-            users.filter((users) => !users.role || users.role === "STUDENT")
-              .length
-          }
-        />
-        <InfoCard
-          icon={CheckCircle}
-          variant="yellow"
-          label="Events Pending Review"
-          numberOfItems={
-            events.filter(
-              (event) => !event.status || event.status === "PENDING"
-            ).length
-          }
-        />
-        <InfoCard
-          icon={Globe}
-          variant="purple"
-          label="Total Events"
-          numberOfItems={totalEvents}
-        />
+        <Sheet>
+          <SheetTrigger>
+            <InfoCard
+              icon={User2Icon}
+              variant="emerald"
+              label="Leaders"
+              numberOfItems={
+                users.filter((users) => !users.role || users.role === "LEADER")
+                  .length
+              }
+            />
+          </SheetTrigger>
+          <SheetContent className=" ">
+            <SheetHeader>
+              <SheetTitle>Events Booked</SheetTitle>
+              <SheetDescription>
+                These are the events you have booked
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Email
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {users
+                    .filter((user) => user.role === "LEADER")
+                    .map((user) => (
+                      <tr key={user.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {user.name}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {user.email}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </SheetContent>
+        </Sheet>
+        <Sheet>
+          <SheetTrigger>
+            <InfoCard
+              icon={Globe}
+              variant="purple"
+              label="Total Events"
+              numberOfItems={totalEvents}
+            />
+          </SheetTrigger>
+          <SheetContent className=" ">
+            <SheetHeader>
+              <SheetTitle>Events Booked</SheetTitle>
+              <SheetDescription>
+                These are the events you have booked
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {events.map((event) => (
+                    <tr key={event.id}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {event.name}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {event.date?.toString()}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">
+                          {event.status || "PENDING"}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </SheetContent>
+        </Sheet>
+        <Sheet>
+          <SheetTrigger>
+            <InfoCard
+              icon={Clock}
+              variant="rose"
+              label="Published Events"
+              numberOfItems={
+                events.filter(
+                  (event) => !event.status || event.status === "PUBLISHED"
+                ).length
+              }
+            />
+          </SheetTrigger>
+          <SheetContent className=" ">
+            <SheetHeader>
+              <SheetTitle>Published Events</SheetTitle>
+              <SheetDescription>
+                Events that are published and ready to be booked
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {events
+                    .filter(
+                      (event) =>
+                        event.status === "PUBLISHED" &&
+                        event.date &&
+                        event.date > new Date()
+                    )
+                    .map((event) => (
+                      <tr key={event.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.name}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.date?.toString()}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.status || "PENDING"}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </SheetContent>
+        </Sheet>
 
-        <InfoCard
-          icon={Clock}
-          variant="rose"
-          label="Published Events"
-          numberOfItems={
-            events.filter(
-              (event) => !event.status || event.status === "PUBLISHED"
-            ).length
-          }
-        />
+        <Sheet>
+          <SheetTrigger>
+            <InfoCard
+              icon={CheckCircle}
+              variant="yellow"
+              label="Events Pending Review"
+              numberOfItems={
+                events.filter(
+                  (event) => !event.status || event.status === "PENDING"
+                ).length
+              }
+            />
+          </SheetTrigger>
+          <SheetContent className=" ">
+            <SheetHeader>
+              <SheetTitle>Events Booked</SheetTitle>
+              <SheetDescription>
+                These are the events you have booked
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {events
+                    .filter(
+                      (event) => !event.status || event.status === "PENDING"
+                    )
+                    .map((event) => (
+                      <tr key={event.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.name}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.date?.toString()}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {event.status || "PENDING"}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </SheetContent>
+        </Sheet>
+        <Sheet>
+          <SheetTrigger>
+            <InfoCard
+              icon={User2Icon}
+              variant="rose"
+              label="Students"
+              numberOfItems={
+                users.filter((users) => !users.role || users.role === "STUDENT")
+                  .length
+              }
+            />
+          </SheetTrigger>
+          <SheetContent className=" ">
+            <SheetHeader>
+              <SheetTitle>Events Booked</SheetTitle>
+              <SheetDescription>
+                These are the events you have booked
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Email
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {users
+                    .filter((user) => user.role === "STUDENT")
+                    .map((user) => (
+                      <tr key={user.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {user.name}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {user.email}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
       {/* <Print /> */}
       <Link href="/admin/report">
--- auth-tutorial/app/(dashboard)/(routes)/student/dashboard/page.tsx
+++ auth-tutorial/app/(dashboard)/(routes)/student/dashboard/page.tsx
@@ -5,6 +5,15 @@ import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { CheckCircledIcon } from "@radix-ui/react-icons";
 import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
   ArchiveIcon,
   CheckCircle,
   CirclePower,
@@ -48,14 +57,38 @@ const LeaderPage = async () => {
   return (
     <div className=" p-6 space-y-4">
       <FormSuccess message={"Welcome back, " + user.name + " !"} />
-      <div className=" grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <InfoCard
-          icon={CheckCircle}
-          variant="purple"
-          label="Events Booked"
-          numberOfItems={publishedEvents.length}
-        />
-      </div>
+      <Sheet>
+        <SheetTrigger>
+          <div className=" grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <InfoCard
+              icon={CheckCircle}
+              variant="purple"
+              label="Events Booked"
+              numberOfItems={publishedEvents.length}
+            />
+          </div>
+        </SheetTrigger>
+        <SheetContent className=" ">
+          <SheetHeader>
+            <SheetTitle>Events Booked</SheetTitle>
+            <SheetDescription>
+              These are the events you have booked
+            </SheetDescription>
+          </SheetHeader>
+          <div className=" space-y-4">
+            {publishedEvents.map((event) => (
+              <Link href={`/listing/${event.id}`} key={event.id}>
+                <div className=" flex w-full items-center gap-x-2 m-2 bg-purple-500/15 p-3 rounded-md text-sm text-purple-500 max-w-64">
+                  <StarIcon className=" h-4 w-4" />
+                  <p>{event.name}</p>
+                  <Link2 className=" h-4 w-4" />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </SheetContent>
+      </Sheet>
+
       <Link href="/student/report">
         <Button variant="outline" className="flex items-center m-4">
           <FcDocument className="mr-2" />
